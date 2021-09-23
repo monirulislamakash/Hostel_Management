9575766e6935a42facf67980e82506504ec538c4
Ak@sh <aaj7731@gmail.com>
2021-09-23 01:23:25 -0700

    hostel commit

--- misthostelpro/misthostelpro/settings.py
+++ misthostelpro/misthostelpro/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os 
+import django_heroku
+import dj_database_url
+from decouple import Config
+
+from pathlib import Path
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -48,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'misthostelpro.urls'
@@ -131,3 +139,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATIC_ROOT = "/var/www/example.com/static/"
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
