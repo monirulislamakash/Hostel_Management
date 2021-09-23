0252b89e99f3014523badb6c8bd93563307905e7
Ak@sh <aaj7731@gmail.com>
2021-09-23 02:25:40 -0700

    hostel commit

--- misthostelpro/mist/views.py
+++ misthostelpro/mist/views.py
@@ -2,6 +2,7 @@ from django.http import request
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
+from django.contrib.auth import authenticate
 from .models import *
 from datetime import date
 from .form import *
